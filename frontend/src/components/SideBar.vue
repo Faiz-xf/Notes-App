893b37e6437fe78c4795a57d0a017033d3fd22e2
Faiz-xf <f.khan@xfanatical.com>
2025-05-09 14:04:29 +0530

    rebasing the branch

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -2,25 +2,6 @@
 import { useNotesStore } from '../../stores/notes';
 const store = useNotesStore()
 </script>
-<!-- <template>
-    <aside class="w-[250px] min-h-screen bg-gray-900 text-center p-4">
-
-        <button class="bg-green-800 w-full py-2 rounded-md text-amber-100 my-4 cursor-pointer" @click="store.createNote">New Note</button>
-
-        <ul>
-            <li
-            v-for="note in store.notes"
-            :key="note._id"
-            @click="store.setActiveNote(note._id)"
-            class="py-2 flex justify-between cursor-pointer border-b-2 border-gray-700 hover:bg-gray-600">
-                <span class=" text-amber-50">
-                    {{note.title}}
-                </span>
-                <button class="text-red-400" @click.stop="store.deleteNote(note._id)">Delete</button>
-            </li>
-        </ul>
-    </aside>
-</template> -->
 
 <template>
     <v-btn flat @click="store.createNote">
